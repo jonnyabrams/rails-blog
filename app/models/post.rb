a8cd2d4880a2114b1ed0c9ad61a7423fc8cff802
jonnyabrams <jonnyabrams@hotmail.com>
2022-12-02 15:06:50 +0000

    feat: title/body validation in Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: {minimum: 2, maximum: 50}
+  validates :title, presence: true, length: {minimum: 5, maximum: 50}
+  validates :body, presence: true, length: {minimum: 10, maximum: 1000}
 
 end
