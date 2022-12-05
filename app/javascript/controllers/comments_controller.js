96f27a859615ff6ee1fa2dbf87045f13e1ab4ee1
jonnyabrams <jonnyabrams@hotmail.com>
2022-12-05 09:58:22 +0000

    styles: push comment edit box down not to clash with btn

--- app/javascript/controllers/comments_controller.js
+++ app/javascript/controllers/comments_controller.js
@@ -4,13 +4,13 @@ export default class extends Controller {
   initialize() {}
   connect() {}
   toggleForm(event) {
-    console.log("I clicked edit btn");
     event.preventDefault();
     event.stopPropagation();
     const formID = event.params["form"];
     const commentBodyID = event.params["body"];
     const form = document.getElementById(formID);
     form.classList.toggle("d-none")
+    form.classList.toggle("mt-5")
     const commentBody = document.getElementById(commentBodyID)
     commentBody.classList.toggle("d-none")
   }
