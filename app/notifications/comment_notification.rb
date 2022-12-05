01fedb3e1112611d8e75653d53dca7305e10a233
jonnyabrams <jonnyabrams@hotmail.com>
2022-12-05 14:06:28 +0000

    feat: notifications

--- app/notifications/comment_notification.rb
+++ app/notifications/comment_notification.rb
@@ -6,7 +6,7 @@
 class CommentNotification < Noticed::Base
   # Add your delivery methods
   #
-  # deliver_by :database
+  deliver_by :database
   # deliver_by :email, mailer: "UserMailer"
   # deliver_by :slack
   # deliver_by :custom, class: "MyDeliveryMethod"
@@ -17,11 +17,14 @@ class CommentNotification < Noticed::Base
 
   # Define helper methods to make rendering easier.
   #
-  # def message
-  #   t(".message")
-  # end
-  #
-  # def url
-  #   post_path(params[:post])
-  # end
+  def message
+    @post = Post.find(params[:comment][:post_id])
+    @comment = Comment.find(params[:comment][:id])
+    @user = User.find(@comment.user_id)
+    "#{@user.name} commented on #{@post.title.truncate(10)}"
+  end
+  
+  def url
+    post_path(Post.find(params[:comment][:post_id]))
+  end
 end
