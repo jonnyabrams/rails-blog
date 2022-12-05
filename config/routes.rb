2bb3292b83e24973eec5a10faa6798f0cdd4c633
jonnyabrams <jonnyabrams@hotmail.com>
2022-12-05 14:37:47 +0000

    feat: change route to just /search (was /search/index)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'search/index'
+  get 'search', to: "search#index"
   get 'users/profile'
   devise_for :users, controllers: {
     sessions: "users/sessions",
